1b381a92d9b187be55030f17260c8c7bb3e91ba0
danielmahler1 <danielmahler34@gmail.com>
2024-03-07 21:38:24 -0500

    finished edit mode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
 
 export function EditMode(): JSX.Element {
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [name, setName] = useState("Your Name");
+    const [isStudent, setIsStudent] = useState(true);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <label className="form-switch">
+                Edit Mode:
+                <input
+                    type="checkbox"
+                    checked={isEditMode}
+                    onChange={(e) => setIsEditMode(e.target.checked)}
+                />
+            </label>
+            {isEditMode ? (
+                <div>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Student:
+                        <input
+                            type="checkbox"
+                            checked={isStudent}
+                            onChange={(e) => setIsStudent(e.target.checked)}
+                        />
+                    </label>
+                </div>
+            ) : (
+                <p>
+                    {name} is {isStudent ? "" : "not "}a student
+                </p>
+            )}
         </div>
     );
 }
